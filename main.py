3fd1c6b230d6c71752f5c2ffaeb7c499c629e7b6
Pkrishon <Kpinkins@loyola.edu>
2024-10-06 22:24:36 -0400

    Began code

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-# Code goes here
+# Programmed by Krishon Pinkins
+# Loyola CS151, Professor Zee
+# Due Date: September 18, 2024
+# Programming Assignment: 02
+
+print('Hello challenger!, What is your name?')
+user_name = input()
+
+print('Hello ' + user_name + '! You are in an abandoned high school with your friends and'
+'notice there is a person there acting strange. One of your friends approach this '
+'strange person. They knock them down and begin biting into their flesh. '
+'Do you flee or go to save your friend?')
+
+
